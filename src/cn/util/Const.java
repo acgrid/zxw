1973efa6f9d871044a3a26cd06f6083c8edcd2a9
acgrid <acgrid@gmail.com>
2018-05-29 19:19:26 +0800

    Add VIP price

--- src/cn/util/Const.java
+++ src/cn/util/Const.java
@@ -7,6 +7,8 @@ import java.util.Date;
 public class Const {
    
     public static final String ROOT="/friend/";
+    
+    public static final Integer VIP_FEE_PER_MONTH = 9900; // 单位为分
 
     public static String getCurrentDay(){
     	SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
